78a28bffbec8bfef4b8b8014a606486e7c6775f0
Erasmo Carlos <erasmo.jr1997@gmail.com>
2025-06-15 10:34:04 -0300

    Animate hamburger icon

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,29 @@
 // Este arquivo contém o código JavaScript que adiciona interatividade à página. 
 
-document.addEventListener('DOMContentLoaded', function() {
-    const ctaButton = document.getElementById('cta-button');
-    
-    if (ctaButton) {
-        ctaButton.addEventListener('click', function() {
-            alert('Obrigado por seu interesse! Em breve, você receberá mais informações.');
-        });
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  const ctaButton = document.getElementById('cta-button');
 
-    const imageGallery = document.querySelectorAll('.gallery-image');
-    imageGallery.forEach(image => {
-        image.addEventListener('click', function() {
-            const mainImage = document.getElementById('main-image');
-            mainImage.src = this.src;
-        });
+  if (ctaButton) {
+    ctaButton.addEventListener('click', () => {
+      alert('Obrigado por seu interesse! Em breve, você receberá mais informações.');
     });
+  }
 
-    // Menu hamburguer responsivo
-    const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('navLinks');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
-});
-
-
-// Menu responsivo
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.getElementById('navLinks');
-
-if (hamburger && navLinks) {
-  hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+  const imageGallery = document.querySelectorAll('.gallery-image');
+  imageGallery.forEach(image => {
+    image.addEventListener('click', function () {
+      const mainImage = document.getElementById('main-image');
+      mainImage.src = this.src;
+    });
   });
-}
+
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('navLinks');
+
+  if (hamburger && navLinks) {
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+      hamburger.classList.toggle('active');
+    });
+  }
+});
